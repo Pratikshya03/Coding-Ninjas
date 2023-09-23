f1a6c6b7fa337867a5166fa16e62a4e0e18a39ff
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-09-23 22:59:44 +0530

    Print all Divisors of a number

--- Basics of Java/Print all Divisors of a number.java
+++ Basics of Java/Print all Divisors of a number.java
@@ -1,6 +1,5 @@
 /*Problem Statement:Given an integer 'N' return all the divisors of 'N' in ascending order
 For Example:'N' = 5 . 
-
 The divisors of 5 are 1, 5. 
 Detailed explanation ( Input/output format, Notes, Images )
 Sample Input 1 :
@@ -15,7 +14,7 @@ Sample Output 1 :
 1 2 3 6
 Constraints :
 1 <= 'N' <= 10^5
- */
+*/
 
 import java.util.ArrayList;
 import java.util.List;
