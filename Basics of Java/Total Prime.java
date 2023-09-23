0d66ba22992f8b62fa5509535111a303131ba47c
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-09-24 00:03:57 +0530

    Total Prime

--- Basics of Java/Total Prime.java
+++ Basics of Java/Total Prime.java
@@ -11,7 +11,7 @@ Sample Output 1: 4
 Explanation Of Sample Input 1: The prime numbers between 2 and 10 are 2,3,5 and 7
 Sample Input 2: 25
 Sample Output 2: 3 */
-import java.util.* ;
+import java.util.*;
 import java.io.*; 
 import java.util.Scanner;
 class CountPrime {
@@ -32,7 +32,6 @@ class CountPrime {
 		return d;		
 	}
 }
-
 class Solution {
 	public static void main(String args[]) {
 		
