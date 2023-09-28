7b8e75a35799d897831a7c3134ebe72f86c7ba15
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-09-29 00:01:45 +0530

    Reverse The Array

--- Basics of Java/Reverse The Array.java
+++ Basics of Java/Reverse The Array.java
@@ -1,30 +1,18 @@
-/*Problem Statement:
-
-Given an array/list 'ARR' of integers and a position 'M'. You have to reverse the array after that position. Example:
-
-We have an array ARR = {1, 2, 3, 4, 5, 6} and M = 3 , considering e based indexing so the subarray {5, 6} will be reversed and our output array will be {1, 2, 3, 4, 6, 5} .
-
-Detailed explanation (Input/output format, Notes
-
+/*Problem Statement:Given an array/list 'ARR' of integers and a position 'M'. You have to reverse the array after that position.
+Example:We have an array ARR = {1, 2, 3, 4, 5, 6} and M = 3 , considering e based indexing so the subarray {5, 6} will be reversed and our output array will be {1, 2, 3, 4, 6, 5} .
+Detailed explanation (Input/output format)
 Input Format:
-
-The very first line of input contains an integer 'T' denoting the number of test
- cases.
-
+The very first line of input contains an integer 'T' denoting the number of test cases.
 The first line of every test case contains one integer 'N' where 'N' denotes the number of elements and an integer 'M' which denotes after which position the array has to be reversed.
-
 The second line of every test case contains 'N' space-separated integers which denote the elements of input of array/list.
-
 Output Format:
-
 For each test case, return the required 
 You do not need to print anything, it has already been taken care of. Just implement the given function.
-
 Consider 0-based indexing of the array.
-
 Constraints:
-
-1 <= T <= 10 theta<=M<=N<=5^ * 1theta ^ 4 - theta ^ 9 <= ARR[i] <= 10 ^ 9
+1 <= T <= 10 
+theta<=M<=N<=5^ * 1
+theta ^ 4 - theta ^ 9 <= ARR[i] <= 10 ^ 9
   Sample Input 1:
 
 26 2
