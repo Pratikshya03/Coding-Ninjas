a8712eb3b077cdbf819cf1b6e516e7a597641ce1
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-10-02 23:39:13 +0530

    Count Words

--- Basics of Java/Count Words.java
+++ Basics of Java/Count Words.java
@@ -1,4 +1,5 @@
-/*Problem Statement:For a given input string (str), find and return the total number of words present in it.It is assumed that two words
+/*
+Problem Statement:For a given input string (str), find and return the total number of words present in it.It is assumed that two words
 will have only a single space in between. Also, there wouldn't be any leading and trailing spaces in the given input string.
 Constraints:
 theta <= N <= 10 ^ 3 
