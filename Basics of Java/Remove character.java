ac29d05c042a8404111eb95bdd4f2c8782b01eee
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-10-04 00:03:25 +0530

    Remove character

--- Basics of Java/Remove character.java
+++ Basics of Java/Remove character.java
@@ -1,4 +1,4 @@
-/* Problem Statement:For a given string (str) and a character X, write a function to remove all the occurrences of X from the given string and return it.
+/*Problem Statement:For a given string (str) and a character X, write a function to remove all the occurrences of X from the given string and return it.
 The input string will remain unchanged if the given character(X) doesn't exist in the input string.
 Constraints:
 0 <= N <= 10^6
