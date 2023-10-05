365f1bfa2486cc8fe503d1ab4e7e3966ff1bea0b
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-10-05 23:42:18 +0530

    Admission in Ninja Gram

--- Problem_of_the_day/Admission in Ninja Gram.java
+++ Problem_of_the_day/Admission in Ninja Gram.java
@@ -1,11 +1,9 @@
 /*Problem Statement:Ninja is shifting to a new place named NinjaGram. To take admission to the new school, the teacher gives him a sentence and asks him to check whether the sentence is a pangram.
 A word or a sentence is called a pangram if it contains all the English alphabet letters. Since Ninja is new to programming, he doesn't have much experience; he asks you to solve the problem. Can you help Ninja figure out whether the sentence is a pangram?
-
 Detailed explanation (Input/output format):
 The first line of input contains an integer 'T' denoting the number of test cases. The test cases follow.
 The first line of each test case contains a number 'n' denoting the number of characters in the string.
 The second line of each test case contains the string where the string characters can be both uppercase and lowercase.
-
 Output Format:
 For each test case, if Ninja managed to solve the problem, print "YES" else "NO".
 Print the output of each test case in a separate line.
